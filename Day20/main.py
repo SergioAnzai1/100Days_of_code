ff69723e21ea61615fd5465ef9f8ec359280790b
zanzai <sergioanzai@gmail.com>
2025-05-14 19:09:29 -0300

    Implementando a score board na main

--- Day20/main.py
+++ Day20/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = ScoreBoard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -24,12 +26,18 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-
+        scoreboard.increse_score()
     
-    
-    
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.ycor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+        
+        
 screen.exitonclick()
 
 
