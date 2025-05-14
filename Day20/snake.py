75d0cbbc0568ea8a5a25b7ac60aec7020260a658
zanzai <sergioanzai@gmail.com>
2025-05-14 11:45:21 -0300

    começando a fazer o jogo da cobrinha

---
+++ Day20/snake.py
@@ -0,0 +1,47 @@
+from turtle import Turtle
+
+START_POSITION = [(0,0),(-20,0),(-40,0)]
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+    
+    def create_snake(self):    
+        for position in START_POSITION:
+            new_segment = Turtle(shape="square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+            
+            new_segment.setheading
+
+    def move(self): 
+        for seg_num in range(len(self.segments) - 1, 0,-1):
+            new_x = self.segments[seg_num -1].xcor()
+            new_y = self.segments[seg_num -1].ycor()
+            self.segments[seg_num].goto(new_x,new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+    
+    def rigth(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+    
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
