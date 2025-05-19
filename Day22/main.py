e439857bc65d973d4df3e41ce1e547d20542bdf0
zanzai <sergioanzai@gmail.com>
2025-05-19 11:00:51 -0300

    fazendo a bola e detectando colisão com as bordas

--- Day22/main.py
+++ Day22/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle , Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -10,6 +11,8 @@ screen.tracer(0)
 
 l_paddle = Paddle((-350,0))
 r_paddle = Paddle((350,0))
+
+ball = Ball()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -22,7 +25,13 @@ screen.onkey(r_paddle.go_down,"Down")
 game_is_on = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move(False)
+    
+    #detect colision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 screen.exitonclick()
     
