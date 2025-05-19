bbb87b6b660554c901dcdbb87be878cb2a1f3b1a
zanzai <sergioanzai@gmail.com>
2025-05-19 10:38:41 -0300

    arrumando erro de logica da colisão com cauda

--- Day20/main.py
+++ Day20/main.py
@@ -39,7 +39,7 @@ while game_is_on:
         scoreboard.game_over()
     
     #Detect collision with tail.
-    for segment in snake.segments[2::1]:
+    for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
