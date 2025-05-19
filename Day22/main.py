06f379b28710a88e3eda9f71dee422b4015ef030
zanzai <sergioanzai@gmail.com>
2025-05-19 10:42:14 -0300

    começando o projeto Pong

---
+++ Day22/main.py
@@ -0,0 +1,37 @@
+from turtle import Turtle , Screen
+from paddle import Paddle
+import time
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800,height=600)
+screen.title("Pong")
+screen.tracer(0)
+
+l_paddle = Paddle((-350,0))
+r_paddle = Paddle((350,0))
+
+screen.listen()
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down,"s")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down,"Down")
+
+
+
+game_is_on = True
+
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
+    
+
+
+
+
+
+
+# players
+# ball
+# score
