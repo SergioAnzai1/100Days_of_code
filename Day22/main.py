fe1397e5c3c5a7dea0c56d29157d1a56dbf786c4
zanzai <sergioanzai@gmail.com>
2025-06-09 10:49:23 -0300

    adicionando scoreboard ao jogo

--- Day22/main.py
+++ Day22/main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle , Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,8 @@ l_paddle = Paddle((-350,0))
 r_paddle = Paddle((350,0))
 
 ball = Ball()
+
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(l_paddle.go_up, "w")
@@ -36,7 +39,18 @@ while game_is_on:
     #detect colision with rigth paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-
+        
+        
+    #detect if ball pass the right paddle
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+    
+    #detect if ball pass the left paddle
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+        
 screen.exitonclick()
     
 
